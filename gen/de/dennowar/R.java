86a50b8ddfbc7696c510f382908b09183a9648ce
Dennis Schmidt <schmidde@fh-brandenburg.de>
2012-04-16 21:58:11 +0200

    einige Layout-Arbeiten

--- gen/de/dennowar/R.java
+++ gen/de/dennowar/R.java
@@ -13,6 +13,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int logo_b1000=0x7f020001;
+        public static final int win=0x7f020002;
     }
     public static final class id {
         public static final int btn_antwort=0x7f050007;
